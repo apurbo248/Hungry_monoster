b601fa50ecd27bb3614baed1f0f96851df86b08f
apurbo248 <apurbopal248@gmail.com>
2021-02-07 23:59:41 +0600

    almost complete

--- hungry.js
+++ hungry.js
@@ -1,14 +1,36 @@
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => displayFood(data))
 
-const displayFood = categories =>{
-    const div = document.getElementById('allfood');
-    for(let i = 0; i< categories.length; i++){
-        const foodCategories = categories[i];
-        const item = document.createElement('li');
-        item.innerText = foodCategories.strCategory;
-        div.appendChild(item);
-    }
-   
-}
+const imageSrc = document.getElementById('img-src');
+const imageTitle = document.getElementById('img-title');
+const search = document.getElementById('search');
+
+search.addEventListener('click', function(){
+
+    const searchValue= document.getElementById('input-value');
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchValue.value}`)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals));
+
+
+            const displayMeals = meals =>{
+            search.addEventListener('click', function(){
+                const mealsDiv = document.getElementById('meals');
+            
+                meals.forEach(meal => {
+                    const mealDiv = document.createElement('div');
+                    mealDiv.className = 'meals-item';
+                    const contant = `
+                        <img  src="${meal.strMealThumb}" alt="">
+                        <h5>${meal.strMeal}</h5>
+                    `
+                mealDiv.innerHTML = contant;
+                    mealsDiv.appendChild(mealDiv);
+                   
+                });
+                document.getElementById('input-value').value = " ";
+                document.getElementById('search').value = " ";
+            })
+           
+            }
+          
+      
+});
